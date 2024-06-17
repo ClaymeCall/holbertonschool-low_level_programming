351b1e1b2d48ce236bc3f143bb4cadc82cfcf374
Clement Callejon <9397@holbertonstudents.com>
2024-06-17 12:19:57 +0000

    Simplifying the program to print directly the size of the types instead of variables of said types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Prints the size of all variable types for the current machine.
  *
@@ -9,22 +8,13 @@
 int main(void)
 {
 	/**
-	 *  Declaring all variables
-	 */
-	char c;
-	int i;
-	long int li;
-	long long int lli;
-	float f;
-
-	/**
 	 * Printing each variable size in a sentence
 	 */
-	printf("Size of a char: %lu bytes(s)", sizeof(c));
-	printf("Size of an int: %lu bytes(s)", sizeof(i));
-	printf("Size of a long int: %lu bytes(s)", sizeof(li));
-	printf("Size of a long long int: %lu bytes(s)", sizeof(lli));
-	printf("Size of a float: %lu bytes(s)", sizeof(f));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
 
 	return (0);
 }
