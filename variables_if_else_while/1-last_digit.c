64c611d67b6a53ad1ea3f804808872bacfe82392
Clement Callejon <9397@holbertonstudents.com>
2024-06-17 18:51:05 +0000

    First attempt at Task 1 of project C Variables & Loops

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints the last digit of a randomly generated int.
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2u
+	last_digit = n % 10
+
+	printf("Last digit of %d is %d", n, last_digit, end_of_sentence);
+
+	if (n > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if ()
+	{
+		printf("and is 0\n");
+	}
+	else
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+
+
+	return (0);
+}
