327b015b5756052e2d55b1089a8b046164f4cf99
Clement Callejon <9397@holbertonstudents.com>
2024-06-17 12:07:20 +0000

    entering the correct script in the correct files, and correcting various betty-style related mistakes

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,12 +2,29 @@
 
 
 /**
- * main - This function prints something to the console.
+ * main - Prints the size of all variable types for the current machine.
  *
  * Return: Returns nothing after printing
  */
 int main(void)
 {
-	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	/**
+	 *  Declaring all variables
+	 */
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	/**
+	 * Printing each variable size in a sentence
+	 */
+	printf("Size of a char: %lu\n bytes(s)", sizeof(c));
+	printf("Size of an int: %lu\n bytes(s)", sizeof(i));
+	printf("Size of a long int: %lu\n bytes(s)", sizeof(li));
+	printf("Size of a long long int: %lu\n bytes(s)", sizeof(lli));
+	printf("Size of a float: %lu\n bytes(s)", sizeof(f));
+
 	return (0);
 }
