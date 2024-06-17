2d68caaf06192f1bdcfb13edbacc9b02e61e8d9f
Clement Callejon <9397@holbertonstudents.com>
2024-06-17 10:14:49 +0000

    documenting print function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main(void)
+
+/**
+ * main - This function prints something to the console.
+ */
+void main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
