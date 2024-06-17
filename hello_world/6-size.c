457d445e5adf06de61eba88269813745b2f79c6e
Clement Callejon <9397@holbertonstudents.com>
2024-06-17 12:08:27 +0000

    removing line breaks as printf includes them natively

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -20,11 +20,11 @@ int main(void)
 	/**
 	 * Printing each variable size in a sentence
 	 */
-	printf("Size of a char: %lu\n bytes(s)", sizeof(c));
-	printf("Size of an int: %lu\n bytes(s)", sizeof(i));
-	printf("Size of a long int: %lu\n bytes(s)", sizeof(li));
-	printf("Size of a long long int: %lu\n bytes(s)", sizeof(lli));
-	printf("Size of a float: %lu\n bytes(s)", sizeof(f));
+	printf("Size of a char: %lu bytes(s)", sizeof(c));
+	printf("Size of an int: %lu bytes(s)", sizeof(i));
+	printf("Size of a long int: %lu bytes(s)", sizeof(li));
+	printf("Size of a long long int: %lu bytes(s)", sizeof(lli));
+	printf("Size of a float: %lu bytes(s)", sizeof(f));
 
 	return (0);
 }
