afdbd76ab8553f4a6d7e59f3044067829ed0ba06
Clement Callejon <9397@holbertonstudents.com>
2024-06-17 11:24:06 +0000

    First attempt at task 5 of hello world project

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * main - This function prints something to the console.
+ *
+ * Return: Returns nothing after printing
+ */
+int main(void)
+{
+	printf("%s", "with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
