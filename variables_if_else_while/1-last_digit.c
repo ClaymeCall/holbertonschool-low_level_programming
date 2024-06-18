0401ef394218275d394dfa0fa64e99c6b1befcb6
Clement Callejon <9397@holbertonstudents.com>
2024-06-18 07:41:57 +0000

    Corrected conditions to handle == 0 case more reliably

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,13 +22,13 @@ int main(void)
 	{
 		printf("greater than 5\n");
 	}
-	else if (last_digit < 5)
+	else if (last_digit == 0)
 	{
-		printf("less than 6 and not 0\n");
+		printf("0\n")
 	}
 	else
 	{
-		printf("0\n");
+		printf("less than 6 and not 0\n");
 	}
 
 	return (0);
