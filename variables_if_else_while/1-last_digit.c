103e5a528d25baf21ee36afa3d90c993911730e8
Clement Callejon <9397@holbertonstudents.com>
2024-06-18 08:29:23 +0000

    Changed the way of outputting so the whole sentence is in one printf

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,19 +16,17 @@ int main(void)
 	n = rand() - RAND_MAX / 2u;
 	last_digit = n % 10;
 
-	printf("Last digit of %d is %d and is ", n, last_digit);
-
 	if (last_digit > 5)
 	{
-		printf("greater than 5\n");
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
 	else if (last_digit == 0)
 	{
-		printf("0\n");
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
 	{
-		printf("less than 6 and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
 
 	return (0);
