df00d3ab95a2d8f89b67e60b47116dc7fbcae4b9
Clement Callejon <9397@holbertonstudents.com>
2024-06-18 07:25:18 +0000

    Switched ==0 and <5 cases

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,13 +22,13 @@ int main(void)
 	{
 		printf("greater than 5\n");
 	}
-	else if (last_digit == 0)
+	else if (last_digit < 0)
 	{
-		printf("0\n");
+		printf("less than 6 and not 0\n");
 	}
 	else
 	{
-		printf("less than 6 and not 0\n");
+		printf("0\n");
 	}
 
 	return (0);
