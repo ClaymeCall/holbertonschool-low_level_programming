f551197a8e1c80f75290430e2fd95d7c82d6a7d3
Clement Callejon <9397@holbertonstudents.com>
2024-06-20 23:08:15 +0000

    First attempt at task 10 of project 3 week 3

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - additions two integers and returns the result.
+ *@operand_a: first int to add
+ *@operand_b: second int to add
+ *
+ * Return: the sum, as int
+ */
+int add(int operand_a, int operand_b)
+{
+	return (operand_a + operand_b);
+}
