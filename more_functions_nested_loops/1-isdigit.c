3e6809f97fc7c12828ec778226951e5f3a8f8f4c
Clement Callejon <9397@holbertonstudents.com>
2024-06-24 13:50:40 +0000

    First attempt at task 1 of project 1 week 4

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks for digits.
+ *@c: int to compare with ASCII key column
+ *
+ *
+ * Return: 1 if digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
