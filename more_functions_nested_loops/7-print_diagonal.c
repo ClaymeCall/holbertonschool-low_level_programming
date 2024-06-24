283c1adb41f3273e4560c975996dc80a15ebf2de
Clement Callejon <9397@holbertonstudents.com>
2024-06-24 21:16:01 +0000

    Adjusted logic to print line breaks everytime but '\' only when n > 0

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,13 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
-		if (i == j)
+		if (i != 0 && i == j)
 			_putchar('\\');
 
 		_putchar('\n');
