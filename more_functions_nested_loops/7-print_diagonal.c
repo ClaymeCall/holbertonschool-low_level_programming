110d0f0413750e770b7d9f27f338c1310d219c4a
Clement Callejon <9397@holbertonstudents.com>
2024-06-24 20:20:09 +0000

    First attempt at task 7 of project 1 week 4

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_diagonal - print a diagonal line.
+ *@n: integer length of the diagonal to be printed.
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
