eb5933646777a26060a815e23331ac20a221e7ae
Clement Callejon <9397@holbertonstudents.com>
2024-06-24 18:50:54 +0000

    First attempt at task 3 of project 1 week 4

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - print from 0 to 9, than a newline.
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
