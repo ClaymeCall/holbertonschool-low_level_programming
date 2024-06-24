c8fc53730513e0e2d7ec496ce524e3dd91da32fe
Clement Callejon <9397@holbertonstudents.com>
2024-06-24 19:11:01 +0000

    First attempt at task 5 of project 1 week 4

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *more_numbers - print from 0 to 14 ten times, than a newline.
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int number_to_print;
+	char tens;
+	char units;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (number_to_print = 0; number_to_print <= 14; number_to_print++)
+		{
+			tens = (number_to_print / 10) + '0';
+			units = (number_to_print % 10) + '0';
+			if (tens > 0)
+				putchar(tens);
+			_putchar(units);
+
+		}
+	_putchar('\n');
+	}
+}
