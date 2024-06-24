38b46bb2a0d690698673aa8193ed65e4a0d37d74
Clement Callejon <9397@holbertonstudents.com>
2024-06-24 21:08:53 +0000

    Adjusting linebreaking and '\' printing logic

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -11,13 +11,15 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
-		_putchar('\\');
+		if (i == j)
+			_putchar('\\');
+
 		_putchar('\n');
 	}
 }
