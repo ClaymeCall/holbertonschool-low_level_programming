6644d338894dfc832ba42e3910d1742a7d16f5d2
Clement Callejon <9397@holbertonstudents.com>
2024-06-24 18:41:50 +0000

    First attempt at task 2 of project 1 week 4

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - multiplies two integers and returns the result.
+ *@a: first int to multiply
+ *@b: second int to multiply
+ *
+ * Return: the product, as int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
