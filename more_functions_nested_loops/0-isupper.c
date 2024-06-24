c24cbd79c00dd2aa973b889f7d32785c078a4858
Clement Callejon <9397@holbertonstudents.com>
2024-06-24 13:28:37 +0000

    First attempt at task 0 Project 1 week 4

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character.
+ *@c: int to compare with ASCII key column
+ *
+ *
+ * Return: 1 if uppercase, 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
