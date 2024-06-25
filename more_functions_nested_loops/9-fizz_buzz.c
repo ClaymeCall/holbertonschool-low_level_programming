00475d6634a3756f52e13bcbc48f4fda1528c5b6
Clement Callejon <9397@holbertonstudents.com>
2024-06-25 15:20:04 +0000

    First attempt at task 9 of project week 4

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *fizz_buzz - prints 1 to 100 and replaces multiples of 3 or 5 by fizz or buzz.
+ *
+ * Return: void
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		else if ((i % 3 != 0) && (i % 5 != 0))
+			printf("%d", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+}
