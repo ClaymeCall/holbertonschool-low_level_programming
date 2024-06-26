ef62b1947a780b4052a6762e121668edc6b0f0e6
Clement Callejon <9397@holbertonstudents.com>
2024-06-26 13:51:37 +0000

    Adding betty style indentation

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,16 @@
  *
  *Return: void.
  */
-
 void print_rev(char *s)
 {
-    int i;
+	int i;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-    }
-    for (i = i - 1; s[i] != '\0'; i--)
-    {
-        _putchar(s[i]);
-    }
-    _putchar('\n');
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (i = i - 1; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
