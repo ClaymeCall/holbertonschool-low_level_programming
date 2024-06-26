f1623aa88134a7a96fdd0a4c18da7f6f50f30cc3
Clement Callejon <9397@holbertonstudents.com>
2024-06-26 12:17:26 +0000

    First attempt at task 3 of project 2 week 4

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_puts - Prints a string followed by a newline to stdout.
+ *@str: The string to print.
+ *
+ *Return: void.
+ */
+
+void _puts(char *str)
+{
+    while (*str)
+    {
+        _putchar(*str);
+        str++;
+    }
+}
