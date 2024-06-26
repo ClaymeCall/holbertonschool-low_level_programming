daf6effb5a88580b54768f9c8b96f8e32af2e476
Clement Callejon <9397@holbertonstudents.com>
2024-06-26 10:03:42 +0000

    First attempt at task 2 of project 2 week 4

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen - Measures the length of a string.
+ *@s: Pointer to the string we want to measure.
+ *
+ * Return: length of string as integer
+ */
+
+void _strlen(char *s)
+{
+	int counter = 0;
+
+	while (*s)
+	{
+		s++;
+		counter++;
+	}
+	return (counter);
+}
