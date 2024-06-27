cf6d6d931ec9d893f7eff637794ce0b09e1009a3
Clement Callejon <9397@holbertonstudents.com>
2024-06-27 14:19:48 +0000

    First attempt at task 6 of project 2 week 4

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *puts2 - Prints every even character index of a string.
+ *@str: Pointer to the string to print.
+ *
+ *Return: void.
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+	int i = 0;
+
+	while (str[count])
+		count++;
+
+	printf("%d\n", count);
+	while (str[i])
+	{
+		putchar(str[i]);
+		i += 2;
+	}
+	putchar('\n');
+}
