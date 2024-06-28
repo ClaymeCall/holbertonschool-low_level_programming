a5dcc93700929d48eedd6e1ce317b0e972428873
Clement Callejon <9397@holbertonstudents.com>
2024-06-28 08:40:26 +0000

    First attempt at task 9 of project 2 week 4

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strcpy- Copies one string into another.
+ *@dest: Pointer to the string that will be written.
+ *@src: Pointer to the string that will be copied.
+ *
+ * Return: The pointer to dest 
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while(*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	dest = '\0';
+
+	return (dest);
+}
+
