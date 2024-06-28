67519dbdc55e4f2f0b79eb46145181a79e6c90d3
Clement Callejon <9397@holbertonstudents.com>
2024-06-28 08:45:22 +0000

    Fixed typos

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,17 +5,17 @@
  *@dest: Pointer to the string that will be written.
  *@src: Pointer to the string that will be copied.
  *
- * Return: The pointer to dest 
+ * Return: The pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	while(*src)
+	while (*src)
 	{
 		*dest = *src;
 		dest++;
 		src++;
 	}
-	dest = '\0';
+	*dest = '\0';
 
 	return (dest);
 }
