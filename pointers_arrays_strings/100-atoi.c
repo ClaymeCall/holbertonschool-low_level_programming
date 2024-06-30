e55b9595c1ab32900a0bacdef18e61e2b1744c83
Clement Callejon <9397@holbertonstudents.com>
2024-06-30 18:07:40 +0000

    First attempt

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,32 +6,28 @@
  */
 int _atoi(char *s)
 {
-	int return_int;
-	int return_int_sign = 1;
-	char *start_of_int;
-        char *end_of_int;
+	int res_int = 0;
+	int res_int_sign = 1;
+	int i = 0;
 
-	int i;
-
-	while (s[i])
+	/* Going through the string until the first convertible char */
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
-		/* Going through the string until the first convertible char */
-		while ((s[i] < '0' || s[i] > '9') || s[i] != '-' || s[i] != '+')
-			i++;
-
 		/* Storing the sign of the future int */
 		if (s[i] < '0' || s[i] > '9')
 		{
-		       if (s[i] == '-' && (s[i] >= '0' && s[i] <= '9'))
-			       return_int_sign = -1;
-		       i++;
+			if (s[i] == '-')
+				res_int_sign *= -1;
 		}
-
-		/* Placing pointer to start and end of the int */
-		start_of_int = &s[i];
-		end_of_int = &s[i];
-		while (*end_of_int >= '0' && *end_of_int <= '9')
-			end_of_int++;
+		i++;
 	}
 
+	/* Convert the number */
+	while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+	{
+		res_int = res_int * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (res_int * res_int_sign);
 }
