0e80e2ae8d0c5cd3354fdfec2fc6dc7519159409
Clement Callejon <9397@holbertonstudents.com>
2024-07-01 21:05:52 +0000

    First attempt at task 3 of project 1 of week 5

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,17 +8,24 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int counter = 0;
+	int still_accept_char;
 	int i;
-	
+
 	while (*s)
 	{
+		still_accept_char = 0;
 		for (i = 0; accept[i]; i++)
 		{
 			if (s == accept[i])
 			{
 				counter++;
+				still_accept_char = 1;
 				break;
 			}
 		}
+		if (still_accept_char == 0)
+			break;
+		s++;
 	}
+	return (counter);
 }
