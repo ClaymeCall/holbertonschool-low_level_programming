3b29b49b17cbe6efdd7decdf04f6a51912d3cb17
Clement Callejon <9397@holbertonstudents.com>
2024-07-01 08:59:12 +0000

    First attempt at Task 0 of project 1 week 5

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+ *_memset - Fills memory with a constant byte.
+ *@s: Pointer to the adress
+ *@b: Byte to fill the memory with.
+ *@n: Number of bytes to fill
+ *
+ * Return: Pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	for (n; n > 0; n--)
+	{
+		*ptr = b;
+		ptr++;
+	}
+	return (s)
+}
