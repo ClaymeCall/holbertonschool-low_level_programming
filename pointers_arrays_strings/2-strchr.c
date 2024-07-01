2caf41af067b8318a0d5d2865bd3934d4cd00ee1
Clement Callejon <9397@holbertonstudents.com>
2024-07-01 12:12:36 +0000

    Fixed loop conditions to avoid seg faults

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+/**
+ *_strchr - Locates a character in a string.
+ *@s: String to search
+ *@c: Character to find
+ *
+ *Return: a pointer to the first occurence of c, or NULL if not found.
+ */
+char *_strchr(char *s, char c) {
+    while (*s && *s != c) 
+        s++;
+
+        return (*s);
+}                
