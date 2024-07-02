85e3aa42ee37af64fc085f80029c15b097adf79f
Clement Callejon <9397@holbertonstudents.com>
2024-07-02 09:45:56 +0000

    First attempt at task 5 of project 1 week 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+/**
+ *_strstr - Locates a substring in a string
+ *@haystack: String to search in
+ *@needle: Substring to locate
+ *
+ *Return: Pointer to the first character of the located substring or NULL.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+	int matching_chars = 0;
+	int needle_len = 0;
+
+	if (*needle == '\0')
+		return (haystack);
+
+	while (needle[i])
+	{
+		needle_len++;
+		i++;
+	}
+
+	for (; *haystack ; haystack++)
+	{
+		if (needle[0] == *haystack)
+		{
+			for (needle[++j]; needle[j]; j++)
+			{
+				if (needle[j] == haystack[j])
+				{
+					matching_chars++;
+					if (matching_chars == needle_len - 1)
+					{
+						return (haystack);
+					}
+				}
+			}
+			matching_chars = 0;
+		}
+	}
+	return ('\0');
+}
