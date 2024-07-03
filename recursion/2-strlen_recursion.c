27ba8a0d252f57ddc64bb03e3ff0d599c2f5c592
Clement Callejon <9397@holbertonstudents.com>
2024-07-03 19:37:24 +0000

    First attempt at task 2 of project 2 week 5

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - Returns the length of a string using recursion
+ *@s: The string to measure
+ *
+ *Return: The length, as int
+ */
+int _strlen_recursion(char *s)
+{
+	return (*s ? 1 + strle,_recursion(s + 1) : 0;
+}
