52f16e4e2528052ca96d7fd09318c0edf0241e4c
Clement Callejon <9397@holbertonstudents.com>
2024-07-03 10:04:11 +0000

    First attempt at task 7 of project 1 week 5

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ *print_diagsums - prints the sums of two diagonals of a 2D array.
+ *@a: 2D array
+ *@size: size of the diagonals we want to sum
+ *
+ *Return: void
+ */
+void print_diagsums(in *a, int size);
+{
+	int i = 0
+	int sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i][i];
+		sum2 += a[size - i - 1][size - i - 1];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
