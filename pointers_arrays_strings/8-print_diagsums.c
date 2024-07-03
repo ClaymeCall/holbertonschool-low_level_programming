0c5ca54e52fa705af8d339eb9fc794e272ea2e6d
Clement Callejon <9397@holbertonstudents.com>
2024-07-03 15:51:04 +0000

    Added standard lib and betty coding style edits

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  *print_diagsums - prints the sums of two diagonals of a 2D array.
  *@a: 2D array
@@ -14,9 +15,9 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		sum_a += a[i * size + i];
-		sum_b += a[size*size - i * size - j];
+		sum_b += a[size * size - i * size - j];
 		j--;
-	} 
+	}
 
 	printf("%d, %d\n", sum_a, sum_b);
 }
