032725db4f9f3ee8e9c92e553afd49ad00658e8c
Clement Callejon <9397@holbertonstudents.com>
2024-07-03 15:47:48 +0000

    Refactored to patch var declaration errors and simplify logic

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,19 +1,22 @@
 /**
  *print_diagsums - prints the sums of two diagonals of a 2D array.
  *@a: 2D array
- *@size: size of the diagonals we want to sum
+ *@size: size of one sub-array
  *
  *Return: void
  */
 void print_diagsums(int *a, int size)
 {
-	int i = 0
-	int sum1 = 0, sum2 = 0;
+	int i = 0;
+	int j = 2;
+	int sum_a = 0, sum_b = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += a[i][i];
-		sum2 += a[size - i - 1][size - i - 1];
-	}
-	printf("%d, %d\n", sum1, sum2);
+		sum_a += a[i * size + i];
+		sum_b += a[size*size - i * size - j];
+		j--;
+	} 
+
+	printf("%d, %d\n", sum_a, sum_b);
 }
