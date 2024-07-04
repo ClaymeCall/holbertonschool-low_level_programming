fbad414bd9911808955b4ebfc257c4926fd76fce
Clement Callejon <9397@holbertonstudents.com>
2024-07-04 14:32:05 +0000

    First attempt at task 0 of project 3 week 5

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+/**
+ *main - prints its own name.
+ *@argc: Count of arguments passed in main.
+ *@argv: Array of all the arguments passed in main
+ *
+ *Return: 0 if success, 1 if failure, as int.
+ */
+int main(int argc, char *argv[1])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
