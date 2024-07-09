ec51927655e29fda5be4f850ae3157b7468b7f00
Clement Callejon <9397@holbertonstudents.com>
2024-07-09 11:43:54 +0200

    First attempt at task 1 of project 1 week 6

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strdup - Copies a string to a newly allocated memory space
+ *@str: String to copy
+ *
+ *Return: A char pointer to the string copy.
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+		i++;
+
+	char *array = malloc((i + 1) * sizeof(char));
+
+	if (array == NULL)
+		return (0);
+
+	for (i = 0; str[i] != '\0'; i++)
+		array[i] = str[i];
+
+	return (array);
+}
