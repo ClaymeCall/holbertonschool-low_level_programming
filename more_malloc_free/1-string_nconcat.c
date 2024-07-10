9507eb62309942ad121f2e2ed29d48dd138c9a1c
Clement Callejon <9397@holbertonstudents.com>
2024-07-10 22:06:52 +0000

    Changed signedness of other variables to integrate with function argument

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,9 +10,9 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int len1 = 0, len2 = 0;
+	unsigned int len1 = 0, len2 = 0;
 	char *res_str;
-	int i = 0, j = 0;
+	unsigned int i = 0, j = 0;
 
 	while (s1 && s1[len1])
 		len1++;
