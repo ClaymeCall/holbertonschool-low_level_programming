c5df60ac5ee83ef16b03832d5dcaa8ff64596770
Clement Callejon <9397@holbertonstudents.com>
2024-07-10 22:05:41 +0000

    First attempt at task 1 of project 2 week 6

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ *string_nconcat - Concatenates n bytes of a string to another, to heap memory.
+ *@s1: String 1
+ *@s2: String 2
+ *@n: number of bytes of s2 to concatenate, as unsigned int.
+ *
+ *Return: Pointer to the newly allocated concatenation of the two strings
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len1 = 0, len2 = 0;
+	char *res_str;
+	int i = 0, j = 0;
+
+	while (s1 && s1[len1])
+		len1++;
+
+	while (s2 && s2[len2])
+		len2++;
+
+	res_str = malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (res_str == NULL)
+		return (NULL);
+
+	if (s1)
+	{
+		while (i < len1)
+		{
+			res_str[i] = s1[i];
+			i++;
+		}
+	}
+	if (s2)
+	{
+		while (i < len1 + n)
+		{
+			res_str[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+
+	res_str[i] = '\0';
+
+	return (res_str);
+}
