94bb664a73aa80e4e6d5b55619f2928c1773ccae
Clement Callejon <9397@holbertonstudents.com>
2024-07-10 13:58:47 +0000

    First attempt at task 4 of project 1 week 6

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *free_grid - Frees a 2D array of ints.
+ *@grid: Pointer to the 2D array
+ *@height: Height of the array
+ *
+ *Return: void
+ */
+void **free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid == NULL)
+		return (void);
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
