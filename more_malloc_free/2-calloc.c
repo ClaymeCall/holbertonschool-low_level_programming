475c196779992c3f6be8a7090ff80b65c7c5b6d3
Clement Callejon <9397@holbertonstudents.com>
2024-07-11 12:02:25 +0200

    Fixed logic of for loop to iterate the correct number of bytes

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -20,8 +20,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (array == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		array[i] = 0;
 
-	return ((void *)array);
+	return (array);
 }
