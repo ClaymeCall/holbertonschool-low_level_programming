82ef479f876a1b3a10ad598328f46bb1b54ba8ee
Clement Callejon <9397@holbertonstudents.com>
2024-07-11 11:27:08 +0200

    First attempt at task 3 of project 2 week 6

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *array_range - Creates an array of all integers between to values.
+ *@min: Minimum value to include
+ *@max: Maximum value to include
+ *
+ *Return: Pointer to newly created array, or NULL if error.
+ */
+int *array_range(int min, int max);
+{
+	int i;
+	int *array;
+
+	if (min > max)
+		return (NULL);
+
+	array = malloc((max - min) * sizeof(int));
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; min + i <= max; i++)
+	{
+		array[i] = min + i;
+	}
+	return (array);
+}
