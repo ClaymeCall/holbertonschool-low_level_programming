2b9a27c8cc19f97d21dee775773f01a88f4f5426
Clement Callejon <9397@holbertonstudents.com>
2024-07-11 15:17:32 +0200

    Added header file for struct dog, and README for the folder

---
+++ structures_typedef/README.md
@@ -0,0 +1,3 @@
+# C - Structures / Typedef
+
+This folder hosts all files for the third project of week 6 of Holberton School.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - Type containing all infos relative to a dog
+ *@name: Name of the dog, as char *
+ *@age: Age of the dog, as float
+ *@owner: Name of the owner, as char *
+ *
+ *Description: 
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
