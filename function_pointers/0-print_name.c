7fb3095ddaf0fb4d0f1552d37cf7b1d7d864b368
Clement Callejon <9397@holbertonstudents.com>
2024-07-15 14:32:35 +0200

    Added header file and First attempt at task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_name - Function that prints a name.
+ *@name: Name to print.
+ *@f: Pointer to the function that prints the name.
+ *
+ *Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
