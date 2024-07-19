bbe624aecb07508ae7d4883f82794c1c9530cb1b
Clement Callejon <9397@holbertonstudents.com>
2024-07-19 10:49:57 +0200

    Removed betty coding style mistakes

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,12 +11,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i;
-	
+
 	va_start(list, n);
 
 	for (i = 0; i < n - 1; i++)
 	{
-		printf("%d%s",va_arg(list, int), separator);
+		printf("%d%s", va_arg(list, int), separator);
 	}
 
 	printf("%d\n", va_arg(list, int));
