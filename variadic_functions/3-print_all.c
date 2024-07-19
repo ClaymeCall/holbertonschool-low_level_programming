f0708df3c61fad99013e1dc20e883ea95b56f7fa
Clement Callejon <9397@holbertonstudents.com>
2024-07-19 16:40:04 +0200

    Refactored variable declaration and added stddef library in header file

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,8 +10,7 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str;
-	char *separator;
+	char *str, *separator;
 
 	va_start(args, format);
 	while (format[i] && format)
