5873831fb4f2991698dc768ed9d2e88ac63eb526
Clement Callejon <9397@holbertonstudents.com>
2024-07-19 10:57:04 +0200

    Added handling of the separator argument being a NULL pointer

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -12,12 +12,27 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 
+
+
 	va_start(list, n);
 
-	for (i = 0; i < n - 1; i++)
+	if (separator != NULL)
 	{
-		printf("%d%s", va_arg(list, int), separator);
+		for (i = 0; i < n - 1; i++)
+		{
+
+			printf("%d%s", va_arg(list, int), separator);
+		}
 	}
+	else
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+
+			printf("%d", va_arg(list, int));
+		}
+	}
+
 
 	printf("%d\n", va_arg(list, int));
 
