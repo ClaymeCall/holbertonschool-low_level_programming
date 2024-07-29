ee5831fe4c81da715732b3c1c7205e93a34470bc
Clement Callejon <9397@holbertonstudents.com>
2024-07-29 14:40:36 +0200

    Added a function that prints data from each node of a list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *print_list - Prints all the elements of a 'list_t' list
+ *@h: First node of the list
+ *
+ *Return: The count of nodes in the list
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	int node_count = 0;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		printf("[%d] %s\n", current->len, current->str);
+
+		current = current->next;
+
+		node_count++;
+	}
+
+	return (node_count);
+}
