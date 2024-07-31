c6cfe02689dd11f04a9d1df9d62e52515c805ad6
Clement Callejon <9397@holbertonstudents.com>
2024-07-31 14:21:52 +0200

    Added a function that frees all nodes in a list, and refactored the equivalent one from previous project

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * free_list - Frees all the nodes of a list
+ * free_dlistint - Frees all the nodes of a list
  * @head: Pointer to the head of the list
  *
  * Return: void
  */
-void free_list(list_t *head)
+void free_dlistint(dlistint_t *head)
 {
-	list_t *tmp_ptr;
+	dlistint_t *tmp_ptr;
 
 	while (head != NULL)
 	{
 		tmp_ptr = head;
 		head = head->next;
 
+		/* free the value inside n */
 		free(tmp_ptr);
 	}
 }
