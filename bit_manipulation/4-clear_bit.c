9f54fb8fc4cfcb3921a1422da38eb33c705aec3b
Clement Callejon <9397@holbetonstudents.com>
2024-08-02 00:25:00 +0200

    Added a function that clears a bit at a given index

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *clear_bit - Sets a bit to 0 at a given index in a number.
+ *@n: Pointer to the number, as unsigned long int *.
+ *@index: Index of the bit to set to 0.
+ *
+ *Return: 1 if it worked, -1 if not.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (n == NULL || index > sizeof(n) * 8)
+		return (-1);
+
+	mask <<= index;
+	
+	*n = *n & ~mask;
+
+	return (1);
+}
