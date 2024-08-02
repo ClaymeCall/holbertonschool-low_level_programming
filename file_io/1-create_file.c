bd41d4601cd5a80a132dadf20e65f95f5c6b70ac
Clement Callejon <9397@holbetonstudents.com>
2024-08-02 15:17:06 +0200

    Small refactor for clarity

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,13 +10,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t fd;
-	char *empty_string = "";
+	char *empty_str = "";
 	int text_len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		text_content = empty_string;
+		text_content = empty_str;
 	else
 	{
 		while (text_content[text_len])
@@ -25,9 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-	{
 		return (-1);
-	}
 
 	if (write(fd, text_content, text_len) == -1)
 	{
